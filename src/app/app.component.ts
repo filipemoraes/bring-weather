1e4251744764bb9447048dfe41fd02867e5f2317
Filipe Moraes <filipe-m-pereira@telecom.pt>
2019-09-28 19:43:47 +0100

    Assign responsibilities correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,5 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private apiService: ApiService) {
-    this.apiService
-      .getCityForecast(524901) // TODO dynamic lack
-      .subscribe(data => console.log(data), error => console.error(error)); // TODO show a user friendly message
-  }
+  constructor() {}
 }
--- src/app/components/city-weather-list/city-weather-list.component.ts
+++ src/app/components/city-weather-list/city-weather-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-city-weather-list',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-weather-list.component.scss']
 })
 export class CityWeatherListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private apiService: ApiService) {
+    this.apiService
+      .getCityForecast(524901) // TODO dynamic lack
+      .subscribe(data => console.log(data), error => console.error(error)); // TODO show a user friendly message
   }
 
+  ngOnInit() {}
 }
