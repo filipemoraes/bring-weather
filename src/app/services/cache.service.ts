49c311dbea4fb290a0a87d55e211c6bcead649f2
Filipe Moraes <filipe-m-pereira@telecom.pt>
2019-09-29 13:06:23 +0100

    Create initial cache structure

---
+++ src/app/services/cache.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ICurrentWeatherCache } from '../models/cache.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheService {
+  constructor() {}
+
+  // TODO guardar um item por cada typo e cidade e dar a data de vencimento de cada um 10 minutos
+
+  public getCurrentWeatherCache(cities: number[]): ICurrentWeatherCache {
+    const cache: ICurrentWeatherCache = { cached: [], list: [] };
+    cities.map(city => {
+      const data = this.getCityWeather(city);
+      if (data) {
+        cache.list.push(data);
+        cache.cached.push(city);
+      }
+    });
+
+    return cache;
+  }
+
+  private getCityWeather(city: number): null | any {
+    return null;
+  }
+}
