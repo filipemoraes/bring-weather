cd68ac124367a99231cd4a6697a786808d17568b
Filipe Moraes <filipe-m-pereira@telecom.pt>
2019-09-30 08:06:48 +0100

    Install ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { CityWeatherListComponent } from './components/city-weather-list/city-weather-list.component';
 import { CityWeatherComponent } from './components/city-weather/city-weather.component';
@@ -15,8 +16,9 @@ import { WeatherForecastComponent } from './components/weather-forecast/weather-
     WeatherForecastListComponent,
     WeatherForecastComponent
   ],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, NgbModule],
+  providers: [NgbActiveModal],
+  bootstrap: [AppComponent],
+  entryComponents: [WeatherForecastListComponent]
 })
 export class AppModule {}
